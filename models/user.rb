96bf053cf21b1844748ea8edf5926f279b09995b
Bruno Romero <brunoromero860@gmail.com>
2022-05-31 17:19:59 -0300

    Modificacion de user.rb

--- models/user.rb
+++ models/user.rb
@@ -9,4 +9,11 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_digest = @password
   end
+  def name
+    @name ||= Name.new(name_digest)
+  end
+  def name=(new_name)
+    @name = Name.create(new_name)
+    self.name_digest = @name
+  end
 end
