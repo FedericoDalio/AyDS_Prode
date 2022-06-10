9fcb7ea952e47103c03156c50953600ac63c6001
Bruno Romero <brunoromero860@gmail.com>
2022-06-10 17:32:19 -0300

    modificacion modelo de result

--- models/result.rb
+++ models/result.rb
@@ -1,3 +1,18 @@
 class Result < ActiveRecord::Base
   belongs_to :match
+  after_save :actualizarPuntaje
+
+  private
+
+  def actualizarPuntaje
+    Forecast.where{match: result.match}.find_each do |p|
+  if (p.local == result.local) && (p.visitor == result.visitor)
+    p.score = 3
+    p.user.score_total = p.user.score_total + 3
+  if ((p.local < p.visitor) && (result.local < result.visitor)) || ((p.local > p.visitor) && (result.local > result.visitor))
+    p.score = 1
+    p.user.score_total = p.user.score_total + 1
+
+  end
+  end
 end
